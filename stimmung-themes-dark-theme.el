9dfb0e3ecf283b5df2f1bc7a28ace3dcee63fb80
motform <love.lagerkvist@protonmail.com>
2022-04-06 09:26:13 +0200

    Improve support for `tab-bar-mode`

--- stimmung-themes-dark-theme.el
+++ stimmung-themes-dark-theme.el
@@ -521,8 +521,8 @@
 
    ;; tab-bar-mode
    `(tab-bar              ((t (:background ,bg1 :foreground ,fg1))))
-   `(tab-bar-tab          ((t (:background ,bg1 :foreground ,fg1 :bold t))))
-   `(tab-bar-tab-inactive ((t (:background ,bg1 :foreground ,fg1))))
+   `(tab-bar-tab          ((t (:background ,bg5 :foreground ,fg1 :box (:line-width 1 :color ,fg5 :style nil)))))
+   `(tab-bar-tab-inactive ((t (:background ,bg1 :foreground ,fg5 :box (:line-width 1 :color ,fg2 :style nil)))))
 
    ;; LaTeX
    `(font-latex-sectioning-0-face ((t (:bold t))))
